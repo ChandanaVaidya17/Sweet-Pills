441fd327a43dcede7fad36c887c930d5237fcbf4
Shrii939 <shridhars145@gmail.com>
2023-04-25 22:21:56 +0530

    takeTest

--- asset/js/takeTest.js
+++ asset/js/takeTest.js
@@ -1,183 +1,153 @@
 const quizData = [
   {
     question: "Are you self-employed?",
-    a: "yes",
-    b: "No",
+    choice: ["yes", "No"],
   },
   {
     question: "Do you have a family history of mental illness?",
-    a: "yes",
-    b: "No",
+    choice: ["yes", "No"],
   },
   {
     question: "Have you sought treatment for a mental health condition?",
-    a: "yes",
-    b: "No",
+    choice: ["yes", "No"],
   },
   {
     question:
       " If you have a mental health condition, do you feel that it interferes with your work?",
-    a: "often",
-    b: "Sometimes",
-    c: "Rarely",
-    d: "Never",
-    e: "Don't know",
+    choice: ["often", "Sometimes", "Rarely", "Never", "Don't know"],
   },
   {
     question: "How many employees does your company or organization have?",
-    a: "1-5",
-    b: "6-25",
-    c: "26-100",
-    d: "100-500",
-    e: "500-1000",
-    f: "1000<",
+    choice: ["1-5", "6-25", "26-100", "100-500", "500-1000", "1000<"],
   },
   {
-    question: "Do you work remotely (outside of an office) at least 50% of the time?",
-    a: "yes",
-    b: "No",
+    question:
+      "Do you work remotely (outside of an office) at least 50% of the time?",
+    choice: ["yes", "No"],
   },
   {
     question: "Is your employer primarily a tech company/organization?",
-    a: "yes",
-    b: "No"
+    choice: ["yes", "No"],
   },
   {
     question: "Does your employer provide mental health benefits?",
-    a: "yes",
-    b: "No",
-    c:"Don't Know"
+    choice: ["yes", "No", "Don't Know"],
   },
   {
     question:
       "Do you know the options for mental health care your employer provides?",
-      a: "yes",
-      b: "No",
-      c:"Not sure"
+    choice: ["yes", "No", "Not sure"],
   },
   {
     question:
       "Has your employer ever discussed mental health as part of an employee wellness program?",
-      a: "yes",
-      b: "No",
-      c:"Don't Know"
+    choice: ["yes", "No", "Don't Know"],
   },
   {
     question:
       "Does your employer provide resources to learn more about mental health issues and how to seek help?",
-      a: "yes",
-      b: "No",
-      c:"Don't Know"
+    choice: ["yes", "No", "Don't Know"],
   },
   {
     question:
       "Is your anonymity protected if you choose to take advantage of mental health or substance abuse treatment resources?",
-      a: "yes",
-      b: "No",
-      c:"Don't Know"
+    choice: ["yes", "No", "Don't Know"],
   },
   {
     question:
       "How easy is it for you to take medical leave for a mental health condition?",
-    a: "Very easy",
-    b: "Some what easy",
-    c: "Some what difficult",
-    d: "Very Difficult",
-    e: "Dont Know"
+    choice: [
+      "Very easy",
+      "Some what easy",
+      "Some what difficult",
+      "Very Difficult",
+      "Dont Know",
+    ],
   },
   {
     question:
       "Do you think that discussing a mental health issue with your employer would have negative consequences?",
-      a: "yes",
-      b: "No",
-      c:"Maybe"
+    choice: ["Yes", "No", "Maybe"],
   },
   {
     question:
       ":Do you think that discussing a physical health issue with your employer would have negative consequences?",
-      a: "yes",
-      b: "No",
-      c:"Maybe"
+    choice: ["Yes", "No", "Maybe"],
   },
   {
     question:
       "Do you think that discussing a physical health issue with your employer would have negative consequences?",
-      a: "yes",
-      b: "No",
-      c:"Maybe"
+    choice: ["Yes", "No", "Maybe"],
   },
   {
     question:
       "Would you be willing to discuss a mental health issue with your coworkers??",
-      a: "yes",
-      b: "No",
-      c:"some of them"
+    choice: ["Yes", "No", "Some of them"],
   },
   {
     question:
       "Would you be willing to discuss a mental health issue with your direct supervisor(s)?",
-      a: "yes",
-      b: "No",
-      c:"some of them"
+    choice: ["Yes", "No", "Some of them"],
   },
   {
     question:
       "Would you bring up a mental health issue with a potential employer in an interview?",
-      a: "yes",
-      b: "No",
-      c:"Maybe"
+    choice: ["Yes", "No", "Maybe"],
   },
   {
     question:
       "Would you bring up a physical health issue with a potential employer in an interview?",
-      a: "yes",
-      b: "No",
-      c:"Maybe"
+    choice: ["Yes", "No", "Maybe"],
   },
- 
+
   {
     question:
       "Do you feel that your employer takes mental health as seriously as physical health?",
-      a: "yes",
-      b: "No",
-      c:"Don't know"
+    choice: ["Yes", "No", "Don't Know"],
   },
   {
     question:
       "Have you heard of or observed negative consequences for coworkers with mental health conditions in your workplace?",
-      a: "yes",
-      b: "No",
+    choice: ["Yes", "No"],
   },
- 
 ];
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+const answerEls = document.getElementsByName("option");
 
-const answers= {}
+function deselectAnswers() {
+  const answerEls = document.querySelectorAll("input[type='radio']");
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
 
+let currentQuestion = 0;
 loadQuiz();
 
 function loadQuiz() {
   deselectAnswers();
 
-  const currentQuizData = quizData[currentQuiz];
+  const questionElement = document.getElementById("question");
+  const formElement = document.getElementById("form");
 
-  questionEl.innerText = currentQuizData.question;
-  a_text.innerText = currentQuizData.a;
-  b_text.innerText = currentQuizData.b;
-  c_text.innerText = currentQuizData.c;
-  d_text.innerText = currentQuizData.d;
-}
+  questionElement.innerText = `${currentQuestion + 1}. ${
+    quizData[currentQuestion].question
+  }`;
 
-function deselectAnswers() {
-  answerEls.forEach((answerEl) => (answerEl.checked = false));
+  const options = quizData[currentQuestion].choice;
+  let formHTML = "";
+
+  for (let i = 0; i < options.length; i++) {
+    formHTML += `
+      <div>
+        <input type="radio" name="option"  value="${i}" id="option${i}" required>
+        <label for="option${i}">${options[i]}</label>
+      </div>
+      <br>
+    `;
+  }
+
+  formElement.innerHTML = formHTML;
+  document.getElementById("option0").checked = true;
 }
 
 function getSelected() {
@@ -187,29 +157,25 @@ function getSelected() {
       answer = answerEl.id;
     }
   });
+
   return answer;
 }
 
+var answerArry = [];
+
 submitBtn.addEventListener("click", () => {
   const answer = getSelected();
+  answerArry.push(answer);
+
+  if((currentQuestion+1) == quizData.length)
+
   if (answer) {
-    if (answer === quizData[currentQuiz].correct) {
-      score++;
-    }
+    currentQuestion++;
 
-    currentQuiz++;
-
-    if (currentQuiz < quizData.length) {
+    if (currentQuestion < quizData.length) {
       loadQuiz();
-    } else {
-      quiz.innerHTML = `
-         <h2>You answered ${score}/${quizData.length} questions correctly</h2>
-
-         <button onclick="location.reload()">Reload</button>
-         `;
     }
+    
   }
+  
 });
-
-
-//
