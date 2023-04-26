ee532fe498a0f47175878c414f478f3c5c5a0b75
Vikram Thunga <shruthavbhat@gmail.com>
2023-04-26 05:35:35 +0530

    django admin models update

--- mind/myproject/myapp/admin.py
+++ mind/myproject/myapp/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import user_id
+from .models import user_id, user_data, prediction, Admin
 
 class usertable (admin.ModelAdmin):
     list_display = ("user_name","password","gender","dob","email")
     list_filter = ("gender","dob",)
 
+admin.site.register(Admin)
 admin.site.register(user_id,usertable)
+admin.site.register(user_data)
+admin.site.register(prediction)
 # Register your models here.
