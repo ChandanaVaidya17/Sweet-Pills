dc83c37c7bd1149bf98c739efcc644757cd407a1
Shrii939 <shridhars145@gmail.com>
2023-09-23 10:02:03 +0530

    take quiz auth removed

--- mind/myproject/venv/bin/activate.fish
+++ mind/myproject/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/shrii/Documents/Sweet-Pills/mind/myproject/venv"
+set -gx VIRTUAL_ENV "/home/shrii/Documents/sw/Sweet-Pills/mind/myproject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
