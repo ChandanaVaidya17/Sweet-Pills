2454fa675d87e20365121cdfec55d94895135796
Vikram Thunga <63503602+vikramgt@users.noreply.github.com>
2023-10-01 10:26:52 +0530

    Revert ".py Changes"

--- mind/myproject/myapp/admin.py
+++ mind/myproject/myapp/admin.py
@@ -1,23 +1,9 @@
 from django.contrib import admin
-from .models import user_id, personal_details, QuizResponse
-from .models import FAQ
-
+from .models import user_id
 
 class usertable (admin.ModelAdmin):
-    list_display = ("user_name", "password", "gender", "dob", "email")
-    list_filter = ("gender", "dob",)
+    list_display = ("user_name","password","gender","dob","email")
+    list_filter = ("gender","dob",)
 
-
-class PersonalDetailsAdmin(admin.ModelAdmin):
-    list_display = ("user_id", "family_history", "treatment",
-                    "mental_health_condition", "phy_health_condition", "comments")
-
-
-# Register your models with their respective admin classes
-admin.site.register(user_id, usertable)
-admin.site.register(personal_details, PersonalDetailsAdmin)
-
+admin.site.register(user_id,usertable)
 # Register your models here.
-
-
-admin.site.register(FAQ)
--- mind/myproject/myapp/urls.py
+++ mind/myproject/myapp/urls.py
@@ -1,13 +1,8 @@
 # myapp/urls.py
 
-from django.urls import path, include
+from django.urls import path
 from .views import QuizResponseView
-from django.contrib import admin
-
 
 urlpatterns = [
     path('my-api-endpoint/', QuizResponseView.as_view(), name='my_api_endpoint'),
-    path('bullying/', include('bullyingapp.urls')),
-
-    
 ]
